65bbd287d31de4d57f3b4baca45fb0c089287c38
Harshthakur <yashthakur0729@example.com>
2025-02-18 00:33:35 +0530

    first commit

---
+++ files/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+    strings: ["Web Development", "Backend Developer", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+const toTop = document.querySelector(".top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
