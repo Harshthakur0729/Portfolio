f71ac6bb9357ae7efbf9743539ebbc849dc605b8
Harshthakur <yashthakur0729@example.com>
2025-06-20 23:53:30 +0530

    update link

--- files/script.js
+++ files/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Web Development", "Backend Developer", "Programmer"],
+    strings: ["Frontend Developer", "Backend Developer", "Programmer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
